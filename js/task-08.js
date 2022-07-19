ebfa13dcfdfecd476f9c211903688d82f3969837
Illya-Bilyk <bilikillya.i@gmail.com>
2022-07-19 23:53:47 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector('.login-form');
 
+const submitFunc = event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === '' || password.value === '') {
+    return alert("You didn't fill all fields, DO THIS NOW !!!");
+  }
+
+  const form = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(form);
+  formRef.reset();
+};
+
+formRef.addEventListener('submit', submitFunc);
