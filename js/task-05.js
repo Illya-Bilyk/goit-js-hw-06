9aadf418d5775d938e67d45421b53292e1222a5c
Illya-Bilyk <bilikillya.i@gmail.com>
2022-07-19 22:31:39 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,8 @@
 const inputRef = document.querySelector('#name-input');
 const nameRef = document.querySelector('#name-output');
+
+const textInput = event => {
+  nameRef.textContent = event.currentTarget.value;
+};
+
+inputRef.addEventListener('input', textInput);
