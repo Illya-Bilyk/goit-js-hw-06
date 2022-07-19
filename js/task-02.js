dbdf01be838962eee4d21b0dc4ffdab8292239ea
Illya-Bilyk <bilikillya.i@gmail.com>
2022-07-19 15:37:57 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const list = ingredients.forEach(element => {
+  const newItem = document.createElement('li');
+
+  newItem.textContent = element;
+  newItem.classList.add('item');
+  ingredientsRef.append(newItem);
+});
